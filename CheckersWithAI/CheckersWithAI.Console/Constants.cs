dd4feb09bddfc66947b991cf8562aa8ff57a3ef4
Uladzislau Kachehin <kocheginvladislav@gmail.com>
2023-06-20 18:17:19 +0200

    Fix letter case fpr 'h'

--- CheckersWithAI/CheckersWithAI.Console/Constants.cs
+++ CheckersWithAI/CheckersWithAI.Console/Constants.cs
@@ -3,7 +3,7 @@
     internal static class Constants
     {
         public static char minLetter = 'A';
-        public static char maxLetter = 'h';
+        public static char maxLetter = 'H';
         public static int minNumber = 0;
         public static int maxNumber = 7;
     }
